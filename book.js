86821f8f2a54b9770ce5d2ba399a2077bdb58c18
chathuri0421 <chathuriwathsala421@gmail.com>
2024-10-16 11:05:44 +0530

    Create book.js

---
+++ book.js
@@ -0,0 +1,20 @@
+// data/books.js
+const books = {
+    "978-3-16-148410-0": {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        reviews: [],
+    },
+    "978-1-56619-909-4": {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        reviews: [],
+    },
+    "978-0-7432-7356-5": {
+        title: "1984",
+        author: "George Orwell",
+        reviews: [],
+    }
+};
+
+module.exports = books;
