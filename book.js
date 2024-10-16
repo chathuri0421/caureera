f40e42949225b34323fe4a438e27a363eb66d1bf
chathuri0421 <chathuriwathsala421@gmail.com>
2024-10-16 11:45:13 +0530

    Update book.js

--- book.js
+++ book.js
@@ -1,20 +1,14 @@
-// data/books.js
-const books = {
-    "978-3-16-148410-0": {
-        title: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
-        reviews: [],
-    },
-    "978-1-56619-909-4": {
-        title: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        reviews: [],
-    },
-    "978-0-7432-7356-5": {
-        title: "1984",
-        author: "George Orwell",
-        reviews: [],
-    }
-};
+let books = {
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+}
 
-module.exports = books;
+module.exports=books;
